fb688989a2597349695c210d6a9d9e0a38ba4a7d
Ahmad-Zahloote <ahmdzahlt1999@gmail.com>
2023-01-15 15:31:57 +0300

    feat: implemented views and serializers for the courses modules

--- pro_edu/courses/models.py
+++ pro_edu/courses/models.py
@@ -11,37 +11,64 @@ from django.utils.translation import gettext as _
 class Course(TimeStampedModel):
     description = models.CharField(max_length=300)
     title = models.CharField(max_length=100)
-    duration = models.CharField(max_length=10)
+    duration = models.IntegerField()
     number_of_enrollments = models.IntegerField()
     image = models.ImageField(upload_to='')
     rate = models.DecimalField(max_digits=2,decimal_places=1)
 
 
+    def get_course_duration(self):
+        for section in Course.sections.all():
+            duration = section.duration
+            hours = duration/3600
+            minutes = (duration % 3600) / 60
+            seconds = duration % 60
+            return {'hours': hours, 'minutes': minutes, 'seconds': seconds}
 
+    def add_section_duration(self,duration):
+        hours = duration.get('hours')
+        minutes = duration.get('minutes')
+        seconds = duration.get('seconds')
+        return {'hours': hours, 'minutes': minutes, 'seconds': seconds}
 
 
 class Section(TimeStampedModel):
 
-    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE,related_name='sections')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    duration = models.CharField(max_length=10)
+    duration = models.BigIntegerField()
+
+    def add_video_duration(self,duration):
+        hours = duration.get('hours')
+        minutes = duration.get('minutes')
+        seconds = duration.get('seconds')
+
+        self.duration += (seconds+(minutes*60)+(hours*3600))
+
+    def get_section_duration(self):
+        for video in Section.videos.all():
+            duration = video.duration
+            hours = duration / 3600
+            minutes = (duration % 3600) / 60
+            seconds = duration % 60
+            return {'hours': hours, 'minutes': minutes, 'seconds': seconds}
 
 class Video(TimeStampedModel):
     title = models.CharField(max_length=100)
     content = models.FileField(upload_to='')
     subtitle = models.FileField(upload_to='')
-    section = models.ForeignKey(Section,on_delete=models.CASCADE)
-    duration = models.CharField(max_length=6)
+    section = models.ForeignKey(Section,on_delete=models.CASCADE,related_name='videos')
+    duration = models.IntegerField()
 
 
-    def get_duration(self):
+    def get_video_duration(self):
         clip = VideoFileClip(self.content)
-        duration = clip.duration
-        minutes = int(duration/60)
-        seconds = int(duration%60)
-        result = str(minutes) + ':' + str(seconds)
-        return result
+        duration = round(clip.duration)
+        hours = duration/3600
+        minutes = (duration%3600)/60
+        seconds = duration%60
+        return {'hours':hours,'minutes':minutes,'seconds':seconds}
 
     def find_between(s, first, last):
         try:
@@ -60,6 +87,6 @@ class Video(TimeStampedModel):
 class Enrollments(models.Model):
     customer = models.OneToOneField('account.Customer',on_delete=models.PROTECT)
     course = models.ForeignKey(Course,on_delete=models.PROTECT)
-    date_of_enrollment = models.DateField(auto_now_add= True)
+    date_of_enrollment = models.DateTimeField(auto_now_add= True)
 
 
